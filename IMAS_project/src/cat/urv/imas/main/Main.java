a1ebd42ec05efc4de68421f7fb12fc418a6f57f4
Jorge Sierra <jsierra.siete@gmail.com>
2018-11-12 08:18:36 +0100

    Standalone Jade Main version

--- IMAS_project/src/cat/urv/imas/main/Main.java
+++ IMAS_project/src/cat/urv/imas/main/Main.java
@@ -10,16 +10,24 @@ import jade.Boot;
 
 public class Main {
     public static void main(String[] args) {
-        // Same as running from the command line jade.Boot -gui
+        /* Without GUI
+        Runtime rt = Runtime.instance();
+        Profile profile = new ProfileImpl();
+        profile.setParameter(Profile.PLATFORM_ID, "Platform Name");
+        profile.setParameter(Profile.CONTAINER_NAME, "Container Name");
+        ContainerController cc = rt.createMainContainer(profile);
+        */
+
+        // With gui, first launch the jade.Boot and then connect to it
         String[] param = new String[1];
         param[0] = "-gui";
         Boot.main(param);
 
         ProfileImpl p = new ProfileImpl(); // "From JADE: configuration-dependent classes and boot parameter"
-        p.setParameter(Profile.CONTAINER_NAME, "IMAS-Container"); // In a separate container ? How to get the main container ?
+        p.setParameter(Profile.CONTAINER_NAME, "IMAS-Container");
         p.setParameter(Profile.LOCAL_HOST, "127.0.0.1");
         p.setParameter(Profile.GUI, "true");
-        ContainerController cc = Runtime.instance().createAgentContainer(p);
+        ContainerController cc = Runtime.instance().createAgentContainer(p);  // createMainContainer
 
         // Add the system agent
         try {
